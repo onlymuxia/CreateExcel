1e223cff0fac014c7a9e4213a9b69b5e2d700fb9
main_admin <main_admin>
2016-12-15 20:15:23 +0800

    单元格颜色

--- ExcelExport/Exceloperator.cs
+++ ExcelExport/Exceloperator.cs
@@ -34,45 +34,45 @@ namespace Onlymuxia.ExcelOperation
                 ICell cell = row.CreateCell(0);
                 wb.SetColumnWidth(0, 20 * 256);
                 cell.SetCellValue("时间");
-                cell.CellStyle = style; 
+                cell.CellStyle = HeadStyle(); 
                 //cell.CellStyle = Getcellstyle(wb, stylexls.头);
                 
                 cell = row.CreateCell(1);
                 cell.SetCellValue("PM2.5");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(2);
                 cell.SetCellValue("PM10");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(3);
                 cell.SetCellValue("TSP");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(4);
                 cell.SetCellValue("风速");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(5);
                 cell.SetCellValue("风速");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(6);
                 cell.SetCellValue("风向");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(7);
                 cell.SetCellValue("温度");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                 cell = row.CreateCell(8);
                 cell.SetCellValue("湿度");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
 
                // wb.SetColumnWidth(9, 15 * 256);
                 cell = row.CreateCell(9);
                 cell.SetCellValue("大气压");
-                cell.CellStyle = style;
+                cell.CellStyle = HeadStyle();
                 Current = 0;
 
 
@@ -108,33 +108,43 @@ namespace Onlymuxia.ExcelOperation
             IRow row = wb.CreateRow(Current);
             ICell cell = row.CreateCell(0);
             cell.SetCellValue(date);
+            cell.CellStyle = TimeDataStyle();
 
             cell = row.CreateCell(1);
             cell.SetCellValue(pm25);
+            cell.CellStyle = TextDataStyle();
 
             cell = row.CreateCell(2);
             cell.SetCellValue(pm10);
+            cell.CellStyle = TextDataStyle();
 
             cell = row.CreateCell(3);
             cell.SetCellValue(tsp);
+            cell.CellStyle = TextDataStyle();
 
             cell = row.CreateCell(4);
             cell.SetCellValue(noise);
+            cell.CellStyle = DigitDataStyle();
 
             cell = row.CreateCell(5);
             cell.SetCellValue(velocity);
+            cell.CellStyle = DigitDataStyle();
 
             cell = row.CreateCell(6);
             cell.SetCellValue(vanes[vane]);
+            cell.CellStyle = TextDataStyle();
 
             cell = row.CreateCell(7);
             cell.SetCellValue(temperature);
+            cell.CellStyle = DigitDataStyle();
 
             cell = row.CreateCell(8);
             cell.SetCellValue(humidity);
+            cell.CellStyle = DigitDataStyle();
 
             cell = row.CreateCell(9);
             cell.SetCellValue(barometric);
+            cell.CellStyle = DigitDataStyle(); 
         }
 
         private Dictionary<int, string> vanes = new Dictionary<int, string>() { { 0, "东北偏北" }, { 1, "东北" }, { 2, "东北偏东" }, { 3, "正东" }, { 4, "东南偏东" }, { 5, "东南" }, { 6, "东南偏南" }, { 7, "正南" }, { 8, "西南偏南" }, { 9, "西南" }, { 10, "西南偏西" }, { 11, "正西" }, { 12, "西北偏西" }, { 13, "西北" }, { 14, "西北偏北" }, { 15, "正北" } };
@@ -142,5 +152,41 @@ namespace Onlymuxia.ExcelOperation
         public HSSFWorkbook hssfworkbook { get; set; }
 
         public ISheet wb { get; set; }
+
+        public ICellStyle TextDataStyle(){
+            IDataFormat dataformat = hssfworkbook.CreateDataFormat(); 
+            ICellStyle style = hssfworkbook.CreateCellStyle();
+            style.Alignment = HorizontalAlignment.Center;
+            return style;
+        }
+        public ICellStyle TimeDataStyle()
+        {
+            IDataFormat dataformat = hssfworkbook.CreateDataFormat();
+            ICellStyle style = hssfworkbook.CreateCellStyle();
+            style.Alignment = HorizontalAlignment.Center;
+            style.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Lime.Index;
+            style.FillPattern = FillPattern.SolidForeground;
+            return style;
+        }
+        public ICellStyle DigitDataStyle()
+        {
+            IDataFormat dataformat = hssfworkbook.CreateDataFormat();
+            ICellStyle style = hssfworkbook.CreateCellStyle();
+            style.Alignment = HorizontalAlignment.Center;
+            style.DataFormat = dataformat.GetFormat("0.0"); //改变小数精度【小数点后有几个0表示精确到小数点后几位】 
+            return style;
+        }
+        public ICellStyle HeadStyle()
+        {
+            IDataFormat dataformat = hssfworkbook.CreateDataFormat();
+            ICellStyle style = hssfworkbook.CreateCellStyle();
+            style.Alignment = HorizontalAlignment.Center;
+            IFont font = hssfworkbook.CreateFont();
+            font.Boldweight = (short)NPOI.SS.UserModel.FontBoldWeight.Bold;
+            style.SetFont(font);//HEAD 样式
+            style.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.DarkYellow.Index;
+            style.FillPattern = FillPattern.SolidForeground;
+            return style;
+        }
     }
 }
